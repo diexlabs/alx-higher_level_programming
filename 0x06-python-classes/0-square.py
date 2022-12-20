f8a0f588aac819639bf5f32ac59f16fdd27df60e
trevordiex <trevordiex@gmail.com>
2022-12-20 21:48:39 +0100

    made scripts executable

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-'''A module containing an empty `Square` klas'''
+'''A module containing an empty `Square` klas
+
+Attributes:
+    None
+'''
 
 class Square:
     """An empty python class"""
