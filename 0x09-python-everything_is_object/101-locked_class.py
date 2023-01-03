b798688c233cf48c4e1bb7d8619e84b11b3a27ea
trevordiex <trevordiex@gmail.com>
2023-01-03 19:02:23 +0100

    commit on advanced task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 '''A module containing `lockedClass` klas definition'''
 
 
 class LockedClass:
     '''A klas that allows for no dynamic attribute except `first_name`'''
 
-    __slots__ = ['first_name',]
+    __slots__ = ['first_name']
