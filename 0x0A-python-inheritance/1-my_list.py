996238332fcd0ef131efa7cbb4f7b58645541832
trevordiex <trevordiex@gmail.com>
2023-01-10 05:41:35 +0100

    still fixing tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,5 @@ class MyList(list):
     '''A subclass of ``list`` with extra public methods'''
 
     def print_sorted(self):
+        '''prints out a list in a sorted order'''
         print(sorted(self))
