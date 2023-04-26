d9e2826ab5cd993bb73a5c941d3d0ed14ce7a8a7
trevordiex <trevordiex@gmail.com>
2023-04-26 08:25:43 +0100

    webscraping task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+
+fs.readFile(filepath, (err, data) => {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log(data.toString());
+	}
+});
+
